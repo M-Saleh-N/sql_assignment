694642333a44f2ebae279740ca57ef2e52b814fa
M-Saleh-N <salehnasser65784@gmail.com>
2025-05-20 22:36:25 +0300

    created an orders table

--- Database.sql
+++ Database.sql
@@ -13,3 +13,9 @@ CREATE TABLE products (
     name VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL CHECK (price >= 0)
 );
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES customers(customer_id) ON DELETE CASCADE,
+    order_date DATE DEFAULT CURRENT_DATE
+);
